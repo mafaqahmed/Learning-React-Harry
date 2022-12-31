f4dbe42c0ef14bc16d711d5e4b3e3b8820bd35ca
Afaq Ahmed <mafaqahmed151@gmail.com>
2022-12-31 16:29:58 +0500

    Enabled React Router (16)
    React router (Home , About)

--- textUtils/src/App.js
+++ textUtils/src/App.js
@@ -1,55 +1,75 @@
 import "./App.css";
-// import About from "./components/About";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Textblock from "./components/Textblock";
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import Alert from "./components/Alert";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
-  const [color, setColor] = useState("white")
+  const [color, setColor] = useState("white");
 
   const toggling = () => {
-    if(mode === "dark") {
+    if (mode === "dark") {
       setMode("light");
-      setColor("light")
-      document.body.style.backgroundColor = "white"
-      showAlert("Light mode is enabled!", "success")
-    }
-    else {
+      setColor("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode is enabled!", "success");
+    } else {
       setMode("dark");
-      setColor("dark")
-      document.body.style.backgroundColor = "#363333"
-      showAlert("Dark mode is enabled!", "success")
+      setColor("dark");
+      document.body.style.backgroundColor = "#363333";
+      showAlert("Dark mode is enabled!", "success");
     }
-  }
+  };
 
   const showAlert = (msg, type) => {
     setAlert({
-      message:msg,
-      type:type
+      message: msg,
+      type: type,
     });
 
     setTimeout(() => {
-      setAlert(null)
+      setAlert(null);
     }, 1500);
-  }
+  };
 
   const handleColor = (event) => {
-    setColor(event.target.value)
+    setColor(event.target.value);
     document.body.style.backgroundColor = `${color}`;
-  }
+  };
 
   return (
     <>
-      <Navbar title="TextUtils" mode = {mode} toggling = {toggling} handleColor = {handleColor} color = {color}/>
-      <Alert alert = {alert}/>
-      <div className="container my-3">
-      <Textblock heading="Enter your text here!" mode = {mode} showAlert = {showAlert} color = {color}/>
-      {/* <About/> */}
-      </div>
+      <Router>
+        <Navbar
+          title="TextUtils"
+          mode={mode}
+          toggling={toggling}
+          handleColor={handleColor}
+          color={color}
+        />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <Textblock
+                  heading="Enter your text here!"
+                  mode={mode}
+                  showAlert={showAlert}
+                  color={color}
+                />
+              }
+            />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- textUtils/src/components/Navbar.js
+++ textUtils/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export default function Navbar({ title = "AfaqAhmed", mode, toggling, handleColor, color }) {
 
@@ -12,9 +13,9 @@ export default function Navbar({ title = "AfaqAhmed", mode, toggling, handleColo
     <div>
       <nav className={`navbar navbar-expand-lg navbar-${mode} bg-${color} text-${color}`}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+          <Link className="navbar-brand" to="/">
             {title}
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -29,14 +30,14 @@ export default function Navbar({ title = "AfaqAhmed", mode, toggling, handleColo
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
+                <Link className="nav-link active" aria-current="page" to="/">
                   Home
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
+                <Link className="nav-link" to="/about">
                   About
-                </a>
+                </Link>
               </li>
             </ul>
             {/* <form className="d-flex" role="search">
