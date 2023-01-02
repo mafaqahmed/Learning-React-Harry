56431078c4ea933960ea35120dafd5421b6dfa44
Afaq Ahmed <mafaqahmed151@gmail.com>
2023-01-02 21:12:02 +0500

    Rectified console error about the logo and improved copy text feature (19)
    text wont be remained selected when we will press copy button

--- textUtils/src/components/Textblock.js
+++ textUtils/src/components/Textblock.js
@@ -23,6 +23,7 @@ export default function Textblock(props) {
   const copyText = () =>{
     navigator.clipboard.writeText(text);
     props.showAlert("Text is copied!", "success");
+    document.getSelection().removeAllRanges();
   };
 
   const removeSpace = ()=>{
