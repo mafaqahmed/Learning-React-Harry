508476db02185c24e1c9be2fafad61b0dc8b32af
Afaq Ahmed <mafaqahmed151@gmail.com>
2023-01-06 22:21:48 +0500

    Fetched News API

--- newsapp/src/components/Navbar.js
+++ newsapp/src/components/Navbar.js
@@ -4,13 +4,13 @@ export class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="/">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">
               NewsApp
             </a>
             <button
-              class="navbar-toggler"
+              className="navbar-toggler"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="/navbarSupportedContent"
@@ -18,17 +18,17 @@ export class Navbar extends Component {
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
-              <span class="navbar-toggler-icon"></span>
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="/">
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/">
                     Home
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/about">
+                <li className="nav-item">
+                  <a className="nav-link" href="/about">
                     About
                   </a>
                 </li>
--- newsapp/src/components/Newsitems.js
+++ newsapp/src/components/Newsitems.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Newsitems extends Component {
+
   render() {
+    let { title, description, imgUrl, newsUrl } = this.props;
     return (
       <div>
-        This is news item
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={imgUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title" >{title?title.slice(0,50):'No title Avaiable'}...</h5>
+            <p className="card-text">{description?description.slice(0,80):'No description avaiable'}...</p>
+            <a href={newsUrl} target="_blank "className="btn btn-primary btn-sm">
+              Read more
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default Newsitems
+export default Newsitems;
