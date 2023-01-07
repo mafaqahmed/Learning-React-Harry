782d440f9a48c0aa740a35756a919ca39c0d5811
Afaq Ahmed <mafaqahmed151@gmail.com>
2023-01-07 16:12:37 +0500

    Added Date, Author and Source

--- newsapp/src/components/Newsitems.js
+++ newsapp/src/components/Newsitems.js
@@ -1,17 +1,28 @@
 import React, { Component } from "react";
 
 export class Newsitems extends Component {
-
   render() {
-    let { title, description, imgUrl, newsUrl } = this.props;
+    let { title, description, imgUrl, newsUrl, author, date,source } = this.props;
     return (
       <div>
         <div className="card">
           <img src={imgUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title" >{title?title.slice(0,50):'No title Avaiable'}...</h5>
-            <p className="card-text">{description?description.slice(0,80):'No description avaiable'}...</p>
-            <a href={newsUrl} target="_blank "className="btn btn-primary btn-sm">
+            <h5 className="card-title">
+              {title ? title.slice(0, 50) : "No title Avaiable"}...
+            </h5>
+            <h6>Source <span className="badge bg-secondary">{source?source:"Not Avaiable"}</span></h6>
+            <p className="card-text">
+              {description
+                ? description.slice(0, 80)
+                : "No description avaiable"}...
+            </p>
+            <p className="card-text"><small className="text-muted">By {author} on {new Date(date).toUTCString()}</small></p>
+            <a
+              href={newsUrl}
+              target="_blank "
+              className="btn btn-primary btn-sm"
+            >
               Read more
             </a>
           </div>
